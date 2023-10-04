31baf5d90182a0a4958e36e46b37872ca127db73
lordgafare <70194534+lordgafare@users.noreply.github.com>
2023-10-04 14:00:55 +0700

    [Add]

---
+++ app/Views/addname.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Addnaja</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/v/dt/dt-1.13.6/datatables.min.css" rel="stylesheet">
+<style>
+    .error{
+        color: red;
+        font-style: italic;
+        font-weight: bold;
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+</style>
+</head>
+
+<body>
+    <div class="container mt-4">
+        <h1>CodeIgniter CRUD</h1>
+        <hr>
+        <div class="mt-3">
+            <form id="add-form" action="<?= site_url('/submit-form'); ?>" method="post" name="addname">
+                <div class="form-group">
+                    <label for="name">
+                        Name
+                    </label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="email">
+                        Email
+                    </label>
+                    <input type="text" name="email" id="email" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <input type="submit" value="Add Data" class="btn btn-success">
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/v/dt/dt-1.13.6/datatables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
+    <script>
+        $(document).ready(function() {
+            if ($('#add-form').length > 0) {
+                $('#add-form').validate({
+                    rules: {
+                        name: {
+                            required: true
+                        },
+                        email: {
+                            required: true,
+                            email: true,
+                            maxlength: 60
+                        }
+                    },
+                    messages: {
+                        name: {
+                            required: 'Please enter name'
+                        },
+                        email: {
+                            required: 'Please enter email',
+                            email: 'Please enter a valid email',
+                            maxlength: 'Maximum length is 60'
+                        }
+                    }
+                });
+            }
+        });
+    </script>
+
+</body>
+
+</html>
