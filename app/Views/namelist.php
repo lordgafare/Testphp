86a1224c9661d0a65d0e56adb3ecf6b829c9bf0c
lordgafare <70194534+lordgafare@users.noreply.github.com>
2023-10-05 17:53:44 +0700

    [Navbar]

--- app/Views/namelist.php
+++ app/Views/namelist.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.datatables.net/v/dt/dt-1.13.6/datatables.min.css" rel="stylesheet">
 
 </head>
-
+<?= $this->include('Layouts/Navbar'); ?>
 <body>
     <div class="container mt-4">
         <h1>CodeIgniter CRUD</h1>
@@ -45,7 +45,7 @@
             </table>
         </div>
     </div>
-
+    <?= $this->include('Layouts/Footer'); ?>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
