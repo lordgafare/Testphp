d1bc315893cff6795d5000e050caed591681f099
lordgafare <70194534+lordgafare@users.noreply.github.com>
2023-10-05 14:11:48 +0700

    [Auth]

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,15 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('hello', 'HelloController::index');
-$routes->get('namelist', 'NameCrud::index');
-$routes->get('addname', 'NameCrud::create');
-$routes->post('submit-form', 'NameCrud::store');
-$routes->get('editname/(:num)', 'NameCrud::singleUser/$1');
-$routes->post('update', 'NameCrud::update');
-$routes->get('delete/(:num)', 'NameCrud::delete/$1');
-$routes->get('register', 'Register::index');
+$routes->get('/hello', 'HelloController::index');
+$routes->get('/namelist', 'NameCrud::index');
+$routes->get('/addname', 'NameCrud::create');
+$routes->post('/submit-form', 'NameCrud::store');
+$routes->get('/editname/(:num)', 'NameCrud::singleUser/$1');
+$routes->post('/update', 'NameCrud::update');
+$routes->get('/delete/(:num)', 'NameCrud::delete/$1');
+$routes->get('/register', 'Register::index');
 $routes->post('/register/save', 'Register::save');
-$routes->get('login', 'Login::index');
+$routes->get('/login', 'Login::index');
 $routes->post('/login/auth', 'Login::auth');
+$routes->get('/logout', 'Login::logout');
 
